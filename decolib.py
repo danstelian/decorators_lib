d18730d9d642208ea8479484794e6ac61682a448
Dan Stelian <stelian.dm@gmail.com>
2018-03-26 12:53:38 +0300

    Homegrown decodators lib

---
+++ decolib.py
@@ -0,0 +1,40 @@
+"""
+Homegrown decorators lib
+@timeit - timing decorator
+"""
+import time
+from functools import wraps
+
+
+timer = time.perf_counter
+
+
+def timeit(reps=1):
+
+    def decorateit(func):
+
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+
+            # total time for number of reps
+            t0 = timer()
+            for i in range(reps):
+                result = func(*args, **kwargs)
+            total_time = timer() - t0
+
+            # best time
+            best_time = 2 ** 32
+            for i in range(reps):
+                t0 = timer()
+                result = func(*args, **kwargs)
+                elapsed = timer() - t0
+                if elapsed < best_time:
+                    best_time = elapsed
+
+            name = func.__name__
+            ret_str = f'{name}: BEST:[{best_time:.8f}s] * {reps} -> TOTAL:[{total_time:.8f}s]'
+            return ret_str, result
+
+        return wrapper
+
+    return decorateit
---
+++ iteration_speed_test.py
@@ -0,0 +1,42 @@
+from decorators_lib import decolib
+
+
+reps = list(range(10000))
+
+
+@decolib.timeit(1000)
+def forloop():
+    res = []
+    for x in reps:
+        res.append(x * x)
+    return res
+
+
+@decolib.timeit(1000)
+def listcomp():
+    return [x * x for x in reps]
+
+
+@decolib.timeit(1000)
+def mapcall():
+    return list(map(lambda x: x * x, reps))
+
+
+@decolib.timeit(1000)
+def genexpr():
+    return list(x * x for x in reps)
+
+
+@decolib.timeit(1000)
+def genfunc():
+    def gen():
+        for x in reps:
+            yield x * x
+    return list(gen())
+
+
+print(forloop()[0])
+print(listcomp()[0])
+print(mapcall()[0])
+print(genexpr()[0])
+print(genfunc()[0])
